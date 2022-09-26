c584791fc25102477abe1608b35dc45e1076dadb
Marcus Johansson <marcusjohansson0111@gmail.com>
2022-09-26 16:00:26 +0200

    Final Commit #4.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { HashRouter as Router, Route, Routes} from "react-router-dom";
 import Info from './Pages/Info';
 import Register from './Pages/Register';
 import Nav from './Resources/myroutes';
